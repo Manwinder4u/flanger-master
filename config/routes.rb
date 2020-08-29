b1a2c8ffa3334b8cc236ac8748110f83c13454f0
Manwinder singh <manwinder@maropost.com>
2020-08-29 17:38:51 +0530

    changing header styling and adding blog and faq page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   root 'instruments#index'
   get 'about/' => "about#index"
   get 'contact/' => "contact#index"
+  get 'blog/' => "blog#index"
+  get 'faq/' => "faq#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
